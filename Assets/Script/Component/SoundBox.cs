16dd1e2d448f2e3c13424c26d3dedfd8c079f61a
coleyoung-game <ighong11@gmail.com>
2025-09-20 22:34:38 +0900

    [Add] SoundBox에 interaction interface 추가 및 임시 로직 추가

--- Assets/Script/Component/SoundBox.cs
+++ Assets/Script/Component/SoundBox.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundBox : MonoBehaviour
+public class SoundBox : MonoBehaviour, IInteractive
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private KeyCode _playKey = KeyCode.Space;
+    [SerializeField] private MeshRenderer _meshRenderer;
+
+    private void Start()
+    {
+        if (!TryGetComponent(out _meshRenderer))
+            Debug.LogError("_meshRenderer is not found.");
+    }
 
     void Update()
     {
@@ -29,5 +36,23 @@ public class SoundBox : MonoBehaviour
                 _audioSource.Play();
             }
         }
+    }
+
+    public void OnGaze()
+    {
+        if(_meshRenderer != null)
+            _meshRenderer.material.color = Color.red;
+    }
+
+    public void OnOutofEye()
+    {
+        if (_meshRenderer != null)
+            _meshRenderer.material.color = Color.gray;
+    }
+
+    public void OnSelect()
+    {
+        if (_meshRenderer != null)
+            _meshRenderer.material.color = Color.blue;
     }
 }
