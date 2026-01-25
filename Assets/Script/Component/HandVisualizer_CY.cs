60dc5d589a2acd8290907466f5610244281256d9
coleyoung-game <ighong11@gmail.com>
2026-01-25 09:39:56 +0900

    [Update] 빌드 세팅이 Vision OS가 아닐 때 Vision OS 관련 API 호출하지 않도록 수정
    - 만약 Unity Pro 이상의 라이센스가 없다면, 빌드 세팅의 타겟 플랫폼을 Vision OS으로 설정하면 안됨.

--- Assets/Script/Component/HandVisualizer_CY.cs
+++ Assets/Script/Component/HandVisualizer_CY.cs
@@ -1,28 +1,30 @@
 // Requires hands package and VisionOSHandExtensions which is only compiled for visionOS and Editor
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Hands;
-#if INCLUDE_UNITY_XR_HANDS && (UNITY_VISIONOS || UNITY_EDITOR)
+#if UNITY_VISIONOS
 using UnityEngine.XR.VisionOS;
-using System.Collections.Generic;
 using UnityEngine.XR.Hands;
 #endif
 
-#if INCLUDE_UNITY_XR_HANDS && (UNITY_VISIONOS || UNITY_EDITOR)
+//#if INCLUDE_UNITY_XR_HANDS && (UNITY_VISIONOS || UNITY_EDITOR)
 public class HandVisualizer_CY : MonoBehaviour
 {
+    [Header("Clap Detection")]
+    [SerializeField] private WorldFixPopupController _worldFixPopupController;
+    [SerializeField] private float _clapThreshold = 0.15f;
+    [SerializeField] private float _clapCooltime = 2.0f;
+    private float _currClapCooltime = 0.0f;
     [SerializeField]
+#if UNITY_VISIONOS
     GameObject m_JointVisualsPrefab;
-    // custom
-    [SerializeField] private GameObject _leftUIObj;
-    //private GameObject _leftUIInstance;
-    //~custom
     XRHandSubsystem m_Subsystem;
     HandGameObjects m_LeftHandGameObjects;
     HandGameObjects m_RightHandGameObjects;
 
 
     static readonly List<XRHandSubsystem> k_SubsystemsReuse = new();
+
+    // Clap Detection Fields
 
     protected void OnEnable()
     {
@@ -64,9 +66,23 @@ public class HandVisualizer_CY : MonoBehaviour
             m_RightHandGameObjects = null;
         }
     }
+#endif
 
     protected void Update()
     {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_worldFixPopupController != null)
+            {
+                if (!_worldFixPopupController.IsActiveCanvas)
+                    _worldFixPopupController.OpenCanvas(Camera.main.transform);
+                else
+                    _worldFixPopupController.CloseCanvas();
+                MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"Clap Detected! UI Active: {_worldFixPopupController.IsActiveCanvas}");
+            }
+        }
+#elif UNITY_VISIONOS
         if (m_Subsystem != null)
         {
             if (m_Subsystem.running)
@@ -98,8 +114,7 @@ public class HandVisualizer_CY : MonoBehaviour
             m_LeftHandGameObjects = new HandGameObjects(
                 Handedness.Left,
                 transform,
-                m_JointVisualsPrefab,
-                _leftUIObj);
+                m_JointVisualsPrefab);
         }
 
         if (m_RightHandGameObjects == null)
@@ -107,14 +122,60 @@ public class HandVisualizer_CY : MonoBehaviour
             m_RightHandGameObjects = new HandGameObjects(
                 Handedness.Right,
                 transform,
-                m_JointVisualsPrefab,
-                null);
+                m_JointVisualsPrefab);
         }
 
         UpdateRenderingVisibility(m_LeftHandGameObjects, m_Subsystem.leftHand.isTracked);
         UpdateRenderingVisibility(m_RightHandGameObjects, m_Subsystem.rightHand.isTracked);
 
+        UpdateRenderingVisibility(m_LeftHandGameObjects, m_Subsystem.leftHand.isTracked);
+        UpdateRenderingVisibility(m_RightHandGameObjects, m_Subsystem.rightHand.isTracked);
+
         SubscribeHandSubsystem();
+#endif
+    }
+#if UNITY_VISIONOS
+    private void CheckClap(XRHandSubsystem subsystem)
+    {
+        if (_currClapCooltime < _clapCooltime)
+        {
+            _currClapCooltime += Time.deltaTime;
+        }
+
+        MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"subsystem: {subsystem}");
+        MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"subsystem.leftHand.isTracked: {subsystem.leftHand.isTracked}");
+        MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"subsystem.rightHand.isTracked: {subsystem.rightHand.isTracked}");
+        if (subsystem != null && subsystem.leftHand.isTracked && subsystem.rightHand.isTracked)
+        {
+            // Palm이 유효하지 않으면 Wrist를 사용 (fallback)
+            var leftJoint = subsystem.leftHand.GetJoint(XRHandJointID.Palm);
+            if (leftJoint.trackingState == XRHandJointTrackingState.WillNeverBeValid)
+                leftJoint = subsystem.leftHand.GetJoint(XRHandJointID.Wrist);
+
+            var rightJoint = subsystem.rightHand.GetJoint(XRHandJointID.Palm);
+            if (rightJoint.trackingState == XRHandJointTrackingState.WillNeverBeValid)
+                rightJoint = subsystem.rightHand.GetJoint(XRHandJointID.Wrist);
+
+            MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"leftJointID: {leftJoint.id}, rightJointID: {rightJoint.id}");
+
+            if (leftJoint.TryGetPose(out Pose leftPose) && rightJoint.TryGetPose(out Pose rightPose))
+            {
+                float distance = Vector3.Distance(leftPose.position, rightPose.position);
+                MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"distance: {distance}");
+                if (distance <= _clapThreshold)
+                {
+                    if (_currClapCooltime >= _clapCooltime)
+                    {
+                        if (!_worldFixPopupController.IsActiveCanvas)
+                            _worldFixPopupController.OpenCanvas(Camera.main.transform);
+                        else
+                            _worldFixPopupController.CloseCanvas();
+                        _currClapCooltime = 0.0f;
+                        MainSystem.Instance.Loggers.LogInfo("HandVisualizer_CY", "CheckClap", $"Clap Detected! UI Active: {_worldFixPopupController.IsActiveCanvas}");
+                    }
+                }
+            }
+        }
     }
 
     void SubscribeHandSubsystem()
@@ -181,6 +242,8 @@ public class HandVisualizer_CY : MonoBehaviour
         if (updateType == XRHandSubsystem.UpdateType.Dynamic)
             return;
 
+        CheckClap(subsystem);
+
         m_LeftHandGameObjects.UpdateJoints(
             subsystem.leftHand,
             (updateSuccessFlags & XRHandSubsystem.UpdateSuccessFlags.LeftHandJoints) != 0);
@@ -199,15 +262,11 @@ public class HandVisualizer_CY : MonoBehaviour
         static Vector3[] s_LinePointsReuse = new Vector3[2];
         const float k_LineWidth = 0.005f;
 
-        //custom
-        GameObject _wristObj;
-        //~custom
 
         public HandGameObjects(
             Handedness handedness,
             Transform parent,
-            GameObject jointVisualsPrefab,
-            GameObject wristObj)
+            GameObject jointVisualsPrefab)
         {
             void AssignJoint(
                 XRHandJointID jointID,
@@ -253,11 +312,6 @@ public class HandVisualizer_CY : MonoBehaviour
 
             AssignJoint((XRHandJointID)VisionOSHandJointID.ForearmWrist, parentTransform);
             AssignJoint((XRHandJointID)VisionOSHandJointID.ForearmArm, parentTransform);
-
-            // custom
-            if (wristObj != null)
-                _wristObj = Instantiate(wristObj);
-            //~custom
         }
 
         public void OnDestroy()
@@ -330,18 +384,7 @@ public class HandVisualizer_CY : MonoBehaviour
                 visuals.gameObject.SetActive(false);
                 return;
             }
-            if (joint.handedness == Handedness.Left)
-            {
-                if (joint.id == XRHandJointID.Wrist)
-                {
-                    Vector3 setPos = visuals.transform.position;
-                    setPos.y += 0.1f;
-                    _wristObj.transform.position = setPos;
-                    // 1. Y축으로 45도 회전하는 '델타(delta)' 쿼터니언을 만듭니다.
-                    Quaternion deltaRotation = Quaternion.Euler(0f, -60f, 0f);
-                    _wristObj.transform.rotation = visuals.transform.rotation * deltaRotation;
-                }
-            }
+
             joint.TryGetVisionOSTrackingState(out var trackingState);
             visuals.SetIsTracked(trackingState);
             var visualsTransform = visuals.transform;
@@ -362,5 +405,5 @@ public class HandVisualizer_CY : MonoBehaviour
             }
         }
     }
-}
 #endif
+}
